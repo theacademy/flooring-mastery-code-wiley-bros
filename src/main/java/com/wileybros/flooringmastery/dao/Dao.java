da44cc13d1fbad09e2e2f3e2bc9bdc0652b4008f
Dan <Mackachooo@gmail.com>
2024-03-22 18:02:10 +0000

    Added getState and getProduct

--- src/main/java/com/wileybros/flooringmastery/dao/Dao.java
+++ src/main/java/com/wileybros/flooringmastery/dao/Dao.java
@@ -1,6 +1,8 @@
 package com.wileybros.flooringmastery.dao;
 
 import com.wileybros.flooringmastery.dto.Order;
+import com.wileybros.flooringmastery.dto.Product;
+import com.wileybros.flooringmastery.dto.State;
 
 import java.time.LocalDate;
 import java.util.Set;
@@ -10,6 +12,9 @@ public interface Dao {
     boolean readData();
     boolean writeData();
     boolean exportData();
+
+    State getState(String abr);
+    Product getProduct(String type);
 
     Set<Order> getOrdersOnDate(LocalDate date);
     boolean distinctID(String id);
--- src/main/java/com/wileybros/flooringmastery/dao/DaoImpl.java
+++ src/main/java/com/wileybros/flooringmastery/dao/DaoImpl.java
@@ -1,6 +1,8 @@
 package com.wileybros.flooringmastery.dao;
 
 import com.wileybros.flooringmastery.dto.Order;
+import com.wileybros.flooringmastery.dto.Product;
+import com.wileybros.flooringmastery.dto.State;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
@@ -21,6 +23,16 @@ public class DaoImpl implements Dao {
     @Override
     public boolean exportData() {
         return false;
+    }
+
+    @Override
+    public State getState(String abr) {
+        return null;
+    }
+
+    @Override
+    public Product getProduct(String type) {
+        return null;
     }
 
     @Override
