31a1ca34ffafa464bbac67088b7e499abc4ffedf
Dan <Mackachooo@gmail.com>
2024-03-22 15:23:16 +0000

    Added formattable print

--- src/main/java/com/wileybros/flooringmastery/ui/UserIO.java
+++ src/main/java/com/wileybros/flooringmastery/ui/UserIO.java
@@ -1,4 +1,7 @@
 package com.wileybros.flooringmastery.ui;
 
 public interface UserIO {
+
+    public void print(String string, Object... args);
+    public void printLn(String string, Object... args);
 }
--- src/main/java/com/wileybros/flooringmastery/ui/UserIOImpl.java
+++ src/main/java/com/wileybros/flooringmastery/ui/UserIOImpl.java
@@ -4,4 +4,12 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UserIOImpl implements UserIO {
+
+    public void print(String string, Object... args) {
+        System.out.printf(string, args);
+    }
+
+    public void printLn(String string, Object... args) {
+        print(string + "\n", args);
+    }
 }
