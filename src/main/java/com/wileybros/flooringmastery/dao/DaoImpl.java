2bee3352a344d9994ce87db14b03e76d61964bc4
Dan <Mackachooo@gmail.com>
2024-03-22 17:33:14 +0000

    Generated Implementations

--- src/main/java/com/wileybros/flooringmastery/dao/DaoImpl.java
+++ src/main/java/com/wileybros/flooringmastery/dao/DaoImpl.java
@@ -1,7 +1,50 @@
 package com.wileybros.flooringmastery.dao;
 
+import com.wileybros.flooringmastery.dto.Order;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.Set;
 
 @Component
 public class DaoImpl implements Dao {
+    @Override
+    public boolean readData() {
+        return false;
+    }
+
+    @Override
+    public boolean writeData() {
+        return false;
+    }
+
+    @Override
+    public boolean exportData() {
+        return false;
+    }
+
+    @Override
+    public Set<Order> getOrdersOnDate(LocalDate date) {
+        return null;
+    }
+
+    @Override
+    public boolean distinctID(String id) {
+        return false;
+    }
+
+    @Override
+    public boolean addOrder(Order order) {
+        return false;
+    }
+
+    @Override
+    public boolean updateOrder(Order order) {
+        return false;
+    }
+
+    @Override
+    public boolean removeOrder(String id) {
+        return false;
+    }
 }
--- src/main/java/com/wileybros/flooringmastery/ui/UserIOImpl.java
+++ src/main/java/com/wileybros/flooringmastery/ui/UserIOImpl.java
@@ -2,14 +2,39 @@ package com.wileybros.flooringmastery.ui;
 
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 @Component
 public class UserIOImpl implements UserIO {
 
+    @Override
     public void print(String string, Object... args) {
         System.out.printf(string, args);
     }
 
+    @Override
     public void printLn(String string, Object... args) {
         print(string + "\n", args);
+    }
+
+    @Override
+    public String readString(String prompt) {
+        return null;
+    }
+
+    @Override
+    public Integer readInt(String prompt) {
+        return null;
+    }
+
+    @Override
+    public BigDecimal readBigDecimal(String prompt) {
+        return null;
+    }
+
+    @Override
+    public LocalDate readLocalDate(String prompt) {
+        return null;
     }
 }
