b81149382f9d4fa27b1457c9fcd6ab365213e6f2
Dan <Mackachooo@gmail.com>
2024-03-22 17:14:48 +0000

    Wrote Interfaces from Class Diagram

--- src/main/java/com/wileybros/flooringmastery/dao/Dao.java
+++ src/main/java/com/wileybros/flooringmastery/dao/Dao.java
@@ -1,4 +1,19 @@
 package com.wileybros.flooringmastery.dao;
 
+import com.wileybros.flooringmastery.dto.Order;
+
+import java.time.LocalDate;
+import java.util.Set;
+
 public interface Dao {
+
+    boolean readData();
+    boolean writeData();
+    boolean exportData();
+
+    Set<Order> getOrdersOnDate(LocalDate date);
+    boolean addOrder(Order order);
+    boolean updateOrder(Order order);
+    boolean removeOrder(String id);
+
 }
--- src/main/java/com/wileybros/flooringmastery/ui/UserIO.java
+++ src/main/java/com/wileybros/flooringmastery/ui/UserIO.java
@@ -1,7 +1,14 @@
 package com.wileybros.flooringmastery.ui;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public interface UserIO {
 
-    public void print(String string, Object... args);
-    public void printLn(String string, Object... args);
+    void print(String string, Object... args);
+    void printLn(String string, Object... args);
+    String readString(String prompt);
+    Integer readInt(String prompt);
+    BigDecimal readBigDecimal(String prompt);
+    LocalDate readLocalDate(String prompt);
 }
