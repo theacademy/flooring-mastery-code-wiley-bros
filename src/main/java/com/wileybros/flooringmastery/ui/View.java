bb434f9dbe9d7924558df8cf0e050dc987e69995
Dan <Mackachooo@gmail.com>
2024-03-25 19:11:45 +0000

    Revert "Revert "Clean up""
    This reverts commit c8af1102b7c6d341f1bbb667eae1783465b83b1e.

--- src/main/java/com/wileybros/flooringmastery/ui/View.java
+++ src/main/java/com/wileybros/flooringmastery/ui/View.java
@@ -3,15 +3,14 @@ package com.wileybros.flooringmastery.ui;
 import com.wileybros.flooringmastery.dto.Order;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDate;
-import java.util.Set;
-
 @Component
 public class View {
 
     private UserIO io = new UserIOImpl();
 
     public int displayMenu() {
+        io.printLn("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+        io.printLn("* <<Flooring Program>>");
         io.printLn("* 1. Display Orders");
         io.printLn("* 2. Add an Order");
         io.printLn("* 3. Edit an Order");
@@ -24,60 +23,4 @@ public class View {
         return io.readInt("Please select from the above choices: ");
     }
 
-    public void welcomeBanner(){
-        io.printLn("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
-        io.printLn("* <<Flooring Program>>");
-    }
-
-    public void displaySuccess(String string){
-        io.printLn("Action Success "+string);
-    }
-
-    public void displayFailure(String string){
-        io.printLn("Action Failure "+string);
-    }
-
-    public void displayError(String string){
-        io.printLn("Error: "+string);
-    }
-
-    public void displayExit(){
-        io.printLn("Goodbye!");
-    }
-
-    public LocalDate askDate(){
-        return io.readLocalDate("Enter a date: ");
-    }
-
-    public LocalDate askFutureDate(){
-        // TODO check that askFutureDate works correctly.
-        LocalDate date;
-        do {
-             date = askDate();
-        } while (LocalDate.now().isBefore(date));
-        return date;
-    }
-
-    public int askOrderID(){
-        return io.readInt("Enter Order Number: ");
-    }
-
-    public Object[] askOrderArgs(){
-        Object[] objs = new Object[4];
-        objs[0] = io.readString("Enter the customer name: ");
-        objs[1] = io.readString("Enter the state abbreviation: ");
-        objs[2] = io.readString("Enter the product type: ");
-        objs[3] = io.readBigDecimal("Enter the area: ");
-        return objs;
-    }
-
-    public void displayOrder(Order order){
-        io.printLn(order.toString());
-    }
-
-    public void displayOrders(Set<Order> orders){
-        for (Order order : orders){
-            io.printLn(order.toString());
-        }
-    }
 }
