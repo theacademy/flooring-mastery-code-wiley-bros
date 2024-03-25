471b32ada1d9af3bf702d19346f6079cb59b3949
Dan <Mackachooo@gmail.com>
2024-03-25 16:59:04 +0000

    Started Controller

--- src/main/java/com/wileybros/flooringmastery/controller/Controller.java
+++ src/main/java/com/wileybros/flooringmastery/controller/Controller.java
@@ -1,15 +1,28 @@
 package com.wileybros.flooringmastery.controller;
 
+import com.wileybros.flooringmastery.service.Service;
+import com.wileybros.flooringmastery.ui.View;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Controller {
+    View view;
+    Service service;
+
+    public Controller(View view, Service service) {
+        this.view = view;
+        this.service = service;
+    }
 
     public void run(){
         boolean running = true;
+        view.welcomeBanner();
         while (running) {
+            switch (view.displayMenu()) {
+                case 1:
 
-            System.out.println("WIP");
+            }
+
 
         }
     }
