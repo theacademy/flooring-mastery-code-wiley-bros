cc58acc53307855b9a1df25391d92871fef31b7d
Dan <Mackachooo@gmail.com>
2024-03-25 11:58:29 +0000

    Implemented DTOs

--- src/main/java/com/wileybros/flooringmastery/dto/Product.java
+++ src/main/java/com/wileybros/flooringmastery/dto/Product.java
@@ -1,4 +1,54 @@
 package com.wileybros.flooringmastery.dto;
 
+import java.math.BigDecimal;
+import java.util.Objects;
+
 public class Product {
+    private String type;
+    private BigDecimal costPSqF;
+    private BigDecimal labourPSqF;
+
+    public Product(String type, BigDecimal costPSqF, BigDecimal labourPSqF) {
+        this.type = type;
+        this.costPSqF = costPSqF;
+        this.labourPSqF = labourPSqF;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public BigDecimal getCostPSqF() {
+        return costPSqF;
+    }
+
+    public BigDecimal getLabourPSqF() {
+        return labourPSqF;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o.getClass() == String.class) return Objects.equals(o, type);
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        return Objects.equals(type, product.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return type != null ? type.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return type + "," + costPSqF + "," + labourPSqF;
+    }
+
+    public static Product parseProduct(String string) {
+        String[] args = string.split(",");
+        return new Product(args[0], new BigDecimal(args[1]), new BigDecimal(args[2]));
+    }
 }
--- src/main/java/com/wileybros/flooringmastery/dto/State.java
+++ src/main/java/com/wileybros/flooringmastery/dto/State.java
@@ -1,4 +1,54 @@
 package com.wileybros.flooringmastery.dto;
 
+import java.math.BigDecimal;
+import java.util.Objects;
+
 public class State {
+    private String abr;
+    private String name;
+    private BigDecimal taxRate;
+
+    public String getAbr() {
+        return abr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getTaxRate() {
+        return taxRate;
+    }
+
+    public State(String abr, String name, BigDecimal taxRate) {
+        this.abr = abr;
+        this.name = name;
+        this.taxRate = taxRate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o.getClass() == String.class) Objects.equals(o, abr);
+        if (o == null || getClass() != o.getClass()) return false;
+
+        State state = (State) o;
+
+        return Objects.equals(abr, state.abr);
+    }
+
+    @Override
+    public int hashCode() {
+        return abr != null ? abr.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return  abr + "," + name + "," + taxRate;
+    }
+
+    public static State parseState(String string) {
+        String[] args = string.split(",");
+        return new State(args[0], args[1], new BigDecimal(args[2]));
+    }
 }
