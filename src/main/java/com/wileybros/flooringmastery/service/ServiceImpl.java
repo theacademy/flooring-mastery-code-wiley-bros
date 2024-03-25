898c81bb467624e8743d4d2378bf8645bb87de10
Dan <Mackachooo@gmail.com>
2024-03-25 18:20:43 +0000

    Revert "Added serviceImpl methods"
    This reverts commit 31171fa0bb17fadd78f6a7bb011f6b678772c1f4.

--- src/main/java/com/wileybros/flooringmastery/service/ServiceImpl.java
+++ src/main/java/com/wileybros/flooringmastery/service/ServiceImpl.java
@@ -1,29 +1,17 @@
 package com.wileybros.flooringmastery.service;
 
-import com.wileybros.flooringmastery.dao.Dao;
 import com.wileybros.flooringmastery.dto.Order;
-import com.wileybros.flooringmastery.dto.Product;
-import com.wileybros.flooringmastery.dto.State;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.Set;
 
 @Component
 public class ServiceImpl implements Service{
-    private Dao dao;
-
-    @Autowired
-    public ServiceImpl(Dao dao) {
-        this.dao = dao;
-    }
-
     @Override
     public Set<Order> getOrdersOnData(LocalDate date) {
-        return dao.getOrdersOnDate(date);
+        return null;
     }
 
     @Override
@@ -41,20 +29,12 @@ public class ServiceImpl implements Service{
 
     @Override
     public boolean updateOrder(Integer id, Object[] args) {
-        String customerName = args[0].toString();
-        String stateAbr = args[1].toString();
-        String productType = args[2].toString();
-        BigDecimal area = new BigDecimal(args[3].toString());
-        State state = dao.getState(stateAbr);
-        Product product = dao.getProduct(productType);
-
-        Order orderToUpdate = new Order(id, customerName, state, product, area, null);
-        return dao.updateOrder(orderToUpdate);
+        return false;
     }
 
     @Override
     public boolean removeOrder(Integer id) {
-        return dao.removeOrder(id);
+        return false;
     }
 
     @Override
