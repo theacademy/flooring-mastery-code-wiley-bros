229e7a670569899ef0b60ba0ed9fc43f4ed8d8bb
AlexEarlatWiley <aearl@wiley.com>
2024-03-25 16:58:29 +0000

    Started implementing controller and view

--- src/main/java/com/wileybros/flooringmastery/controller/Controller.java
+++ src/main/java/com/wileybros/flooringmastery/controller/Controller.java
@@ -1,16 +1,51 @@
 package com.wileybros.flooringmastery.controller;
 
+import com.wileybros.flooringmastery.ui.UserIO;
+import com.wileybros.flooringmastery.ui.UserIOImpl;
+import com.wileybros.flooringmastery.ui.View;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Controller {
 
-    public void run(){
-        boolean running = true;
-        while (running) {
+    private UserIO io = new UserIOImpl();
+    private View view = new View();
 
-            System.out.println("WIP");
+    public void run() {
+        boolean keepGoing = true;
+        int menuSelection = 0;
+        while (keepGoing) {
+
+            menuSelection = getMenuSelection();
+
+            switch (menuSelection) {
+                case 1:
+                    io.printLn("DISPLAY ORDERS");
+                    break;
+                case 2:
+                    io.printLn("ADD AN ORDER");
+                    break;
+                case 3:
+                    io.printLn("EDIT AN ORDER");
+                    break;
+                case 4:
+                    io.printLn("REMOVE AN ORDER");
+                    break;
+                case 5:
+                    io.printLn("EXPORT ALL DATA");
+                    break;
+                case 6:
+                    keepGoing = false;
+                    break;
+                default:
+                    io.printLn("UNKNOWN COMMAND");
+            }
 
         }
+        io.printLn("GOOD BYE");
+    }
+
+    private int getMenuSelection() {
+        return view.displayMenu();
     }
 }
--- src/main/java/com/wileybros/flooringmastery/ui/View.java
+++ src/main/java/com/wileybros/flooringmastery/ui/View.java
@@ -4,4 +4,22 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class View {
+
+    private UserIO io = new UserIOImpl();
+
+    public int displayMenu() {
+        io.printLn("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+        io.printLn("* <<Flooring Program>>");
+        io.printLn("* 1. Display Orders");
+        io.printLn("* 2. Add an Order");
+        io.printLn("* 3. Edit an Order");
+        io.printLn("* 4. Remove an Order");
+        io.printLn("* 5. Export All Data");
+        io.printLn("* 6. Quit");
+        io.printLn("*");
+        io.printLn("* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *");
+
+        return io.readInt("Please select from the above choices: ");
+    }
+
 }
