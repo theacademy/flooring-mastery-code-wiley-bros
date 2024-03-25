1f6863481cc9b5f1d9c14dfdb2b9fedb0bf14c7f
Dan <Mackachooo@gmail.com>
2024-03-25 18:37:08 +0000

    Clean up branch

--- src/main/java/com/wileybros/flooringmastery/controller/Controller.java
+++ src/main/java/com/wileybros/flooringmastery/controller/Controller.java
@@ -12,50 +12,5 @@ public class Controller {
             System.out.println("WIP");
 
         }
-        view.displayExit();
-    }
-
-    private int getMenuSelection() {
-        view.welcomeBanner();
-        return view.displayMenu();
-    }
-
-    private void displayDateSpecificOrders() {
-        LocalDate date = view.askDate();
-        Set<Order> orders = service.getOrdersOnData(date);
-        view.displayOrders(orders);
-    }
-
-    private void addOrder() {
-        LocalDate futureDate = view.askFutureDate();
-        Object[] args = view.askOrderArgs();
-        if (service.addOrder(args, futureDate)) {
-            view.displaySuccess("Order added");
-        } else {
-            view.displayFailure("Order added");
-        }
-    }
-
-    private void editAnOrder() {
-        Integer id = view.askOrderID();
-        Object[] args = view.askOrderArgs();
-        if (service.updateOrder(id, args)) {
-            view.displaySuccess("Order edited");
-        } else {
-            view.displayFailure("Order not edited");
-        }
-    }
-
-    private void removeAnOrder() {
-        Integer id = view.askOrderID();
-        if (service.removeOrder(id)) {
-            view.displaySuccess("Order removed");
-        } else {
-            view.displayFailure("Order not removed");
-        }
-    }
-
-    private void exportAllData() {
-        view.displayFailure("Not Implemented yet");
     }
 }
