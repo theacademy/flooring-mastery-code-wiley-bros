a94eadb11740f4423085a60a5482342383d9e5b0
Dan <mackachooo@gmail.com>
2024-03-26 10:51:49 +0000

    Merge fix

--- src/main/java/com/wileybros/flooringmastery/controller/Controller.java
+++ src/main/java/com/wileybros/flooringmastery/controller/Controller.java
@@ -1,15 +1,54 @@
 package com.wileybros.flooringmastery.controller;
 
+import com.wileybros.flooringmastery.dto.Order;
+import com.wileybros.flooringmastery.service.Service;
+import com.wileybros.flooringmastery.ui.UserIO;
+import com.wileybros.flooringmastery.ui.UserIOImpl;
+import com.wileybros.flooringmastery.ui.View;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.Set;
 
 @Component
 public class Controller {
+    private final View view;
+    private final Service service;
 
-    public void run(){
+    public Controller(View view, Service service) {
+        this.view = view;
+        this.service = service;
+    }
+
+    public void run() {
         boolean running = true;
+        int menuSelection = 0;
         while (running) {
 
-            System.out.println("WIP");
+            menuSelection = getMenuSelection();
+
+            switch (menuSelection) {
+                case 1:
+                    displayDateSpecificOrders();
+                    break;
+                case 2:
+                    addOrder();
+                    break;
+                case 3:
+                    editAnOrder();
+                    break;
+                case 4:
+                    removeAnOrder();
+                    break;
+                case 5:
+                    exportAllData();
+                    break;
+                case 6:
+                    running = false;
+                    break;
+                default:
+                    view.displayError("Input invalid command.");
+            }
 
         }
         view.displayExit();
