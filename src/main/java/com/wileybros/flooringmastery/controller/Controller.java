8ca7fcae74f64b6eeabc72e619fd2b4bc2925515
Dan <mackachooo@gmail.com>
2024-03-26 10:47:55 +0000

    Revert "Merge branch 'main' into mikolaj-service-layer"
    This reverts commit 105852af234189a11992e81f762e1243d2b0f307, reversing
    changes made to ec3642548b30b0acda32b5559ffe38f1185cfce8.

--- src/main/java/com/wileybros/flooringmastery/controller/Controller.java
+++ src/main/java/com/wileybros/flooringmastery/controller/Controller.java
@@ -1,99 +1,16 @@
 package com.wileybros.flooringmastery.controller;
 
-import com.wileybros.flooringmastery.dto.Order;
-import com.wileybros.flooringmastery.service.Service;
-import com.wileybros.flooringmastery.ui.UserIO;
-import com.wileybros.flooringmastery.ui.UserIOImpl;
-import com.wileybros.flooringmastery.ui.View;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDate;
-import java.util.Set;
 
 @Component
 public class Controller {
-    private final View view;
-    private final Service service;
 
-    public Controller(View view, Service service) {
-        this.view = view;
-        this.service = service;
-    }
-
-    public void run() {
+    public void run(){
         boolean running = true;
-        int menuSelection = 0;
         while (running) {
 
-            menuSelection = getMenuSelection();
-
-            switch (menuSelection) {
-                case 1:
-                    displayDateSpecificOrders();
-                    break;
-                case 2:
-                    addOrder();
-                    break;
-                case 3:
-                    editAnOrder();
-                    break;
-                case 4:
-                    removeAnOrder();
-                    break;
-                case 5:
-                    exportAllData();
-                    break;
-                case 6:
-                    running = false;
-                    break;
-                default:
-                    view.displayError("Input invalid command.");
-            }
+            System.out.println("WIP");
 
         }
-        view.displayExit();
-    }
-
-    private int getMenuSelection() {
-        view.welcomeBanner();
-        return view.displayMenu();
-    }
-
-    private void displayDateSpecificOrders() {
-        LocalDate date = view.askDate();
-        Set<Order> orders = service.getOrdersOnData(date);
-        view.displayOrders(orders);
-    }
-
-    private void addOrder() {
-        Object[] args = view.askOrderArgs();
-        if (service.addOrder(args)) {
-            view.displaySuccess();
-        } else {
-            view.displayFailure();
-        }
-    }
-
-    private void editAnOrder() {
-        Integer id = view.askOrderID();
-        Object[] args = view.askOrderArgs();
-        if (service.updateOrder(id, args)) {
-            view.displaySuccess();
-        } else {
-            view.displayFailure();
-        }
-    }
-
-    private void removeAnOrder() {
-        Integer id = view.askOrderID();
-        if (service.removeOrder(id)) {
-            view.displaySuccess();
-        } else {
-            view.displayFailure();
-        }
-    }
-
-    private void exportAllData() {
-        view.displayFailure("Not Implemented yet");
     }
 }
