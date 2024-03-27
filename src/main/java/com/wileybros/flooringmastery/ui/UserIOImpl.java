11af7f22a6e90fee6c58bac3102a5b76fd972330
Dan <mackachooo@gmail.com>
2024-03-27 11:58:40 +0000

    UserIO DOCS

--- src/main/java/com/wileybros/flooringmastery/ui/UserIOImpl.java
+++ src/main/java/com/wileybros/flooringmastery/ui/UserIOImpl.java
@@ -14,22 +14,46 @@ public class UserIOImpl implements UserIO {
 
     final private Scanner scanner = new Scanner(System.in);
 
+    /**
+     * Prints a formatted string to the console.
+     *
+     * @param string the string to be printed
+     * @param args   the arguments to be formatted into the string
+     */
     @Override
     public void print(String string, Object... args) {
         System.out.printf(string, args);
     }
 
+    /**
+     * Prints a formatted string followed by a new line to the console.
+     *
+     * @param string the string to be printed
+     * @param args   the arguments to be formatted into the string
+     */
     @Override
     public void printLn(String string, Object... args) {
         print(string + "\n", args);
     }
 
+    /**
+     * Reads a string input from the user.
+     *
+     * @param prompt the prompt to be displayed to the user
+     * @return the string input from the user
+     */
     @Override
     public String readString(String prompt) {
         print(prompt);
         return scanner.nextLine();
     }
 
+    /**
+     * Reads an integer input from the user.
+     *
+     * @param prompt the prompt to be displayed to the user
+     * @return the integer input from the user, or null if the input is blank
+     */
     @Override
     public Integer readInt(String prompt) {
         String result = readString(prompt);
@@ -37,6 +61,12 @@ public class UserIOImpl implements UserIO {
         return Integer.parseInt(result);
     }
 
+    /**
+     * Reads a BigDecimal input from the user.
+     *
+     * @param prompt the prompt to be displayed to the user
+     * @return the BigDecimal input from the user, or null if the input is blank
+     */
     @Override
     public BigDecimal readBigDecimal(String prompt) {
         String num = readString(prompt);
@@ -44,6 +74,13 @@ public class UserIOImpl implements UserIO {
         return new BigDecimal(num);
     }
 
+    /**
+     * Reads a LocalDate input from the user.
+     *
+     * @param prompt the prompt to be displayed to the user
+     * @param format the DateTimeFormatter to be used for parsing the input
+     * @return the LocalDate input from the user
+     */
     @Override
     public LocalDate readLocalDate(String prompt, DateTimeFormatter format) {
         return LocalDate.parse(readString(prompt), format);
